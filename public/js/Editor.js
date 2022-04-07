829aaf61ce64cae7f99af1fdd5e3bcf6432e8f1e
yinran <yinran@inke.cn>
2022-04-07 10:43:48 +0800

    two Eidtor

--- public/js/Editor.js
+++ public/js/Editor.js
@@ -14,79 +14,106 @@ import { rawContent } from './data/content';
 import TeXEditorExample from './components/TeXEditorExample';
 
 const id = 'draf-tex';
-const contenField = 'raw';
+const contenFieldA = 'rawa';
+const contenFieldB = 'rawb';
+const color = '#f00';
 const HOST = window.location.host.split(':')[0];
 const parmas = {
   user: Math.random().toString(36).substring(7),
 };
 export default function Editor() {
-  const editorRef = React.useRef(null);
+  const editorRefA = React.useRef(null);
+  const editorRefB = React.useRef(null);
   const [isOnline, setOnlineState] = useState(false);
-  const [value, setValue] = useState(null);
-  const [draftBind, setDraftBind] = useState(null);
-  // const [ydoc, provider] = useMemo(() => {
-  //   const ydoc = new Y.Doc();
-  //   // const yRaw = ymap.get(contenField)
-  //   // console.log(yRaw);
-  //   // if(!yRaw) {
-  //   //   ymap.set(contenField, toRawSharedData(value))
-  //   // }
-  //   console.log(ydoc, 'ydoc');
-  //   const provider = new WebsocketProvider(`ws://${HOST}:1234`, id, ydoc, {
-  //     connect: false,
-  //   });
-  //   return [ydoc, provider];
-  // }, [id]);
+  const [valueA, setValueA] = useState(null);
+  const [valueB, setValueB] = useState(null);
+  const [draftBindA, setDraftBindA] = useState(null);
+  const [draftBindB, setDraftBindB] = useState(null);
+  const [ydoc, provider] = useMemo(() => {
+    const ydoc = new Y.Doc();
+    // const yRaw = ymap.get(contenField)
+    // console.log(yRaw);
+    // if(!yRaw) {
+    //   ymap.set(contenField, toRawSharedData(value))
+    // }
+    console.log(ydoc, 'ydoc');
+    const provider = new WebsocketProvider(`ws://${HOST}:1234`, id, ydoc, {
+      connect: false,
+    });
+    return [ydoc, provider];
+  }, [id]);
 
   useEffect(() => {
-    // const draftBind = new DraftBinding({
-    //   ydoc,
-    //   rawPath: contenField,
-    //   editor: editorRef.current,
-    //   provider,
-    //   parmas,
-    //   debug: true,
-    // });
-    // setDraftBind(draftBind);
-    // window.ydoc = ydoc;
-    // window.draftBind = draftBind;
-    // provider.on('status', ({ status }) => {
-    //   setOnlineState(status === 'connected');
-    // });
-    // provider.awareness.setLocalState({
-    //   alphaColor: color.slice(0, -2) + "0.2)",
-    //   color,
-    //   name,
-    // });
-    // provider.on('sync', isSynced => {
-    //   console.log('sync', isSynced);
-    //   if (!isSynced) return;
-    //   // const ymap = ydoc.get(contenField)
-    //   const raw = getRawBySharedData(contenField, ydoc);
-    //   if (raw) {
-    //     setValue(raw);
-    //   } else {
-    //     console.log('initialize');
-    //     const initRaw = value || rawContent;
-    //     setRawToSharedData(contenField, ydoc, initRaw);
-    //   }
-    // });
-    // provider.connect();
-    // return () => {
-    //   draftBind.destroy();
-    //   provider.disconnect();
-    // };
+    const draftBindA = new DraftBinding({
+      ydoc,
+      rawPath: contenFieldA,
+      editor: editorRefA.current,
+      provider,
+      parmas,
+      debug: true,
+    });
+    const draftBindB = new DraftBinding({
+      ydoc,
+      rawPath: contenFieldB,
+      editor: editorRefB.current,
+      provider,
+      parmas,
+      debug: true,
+    });
+    setDraftBindA(draftBindA);
+    setDraftBindB(draftBindB);
+    window.ydoc = ydoc;
+    window.draftBindA = draftBindA;
+    window.draftBindB = draftBindB;
+    provider.on('status', ({ status }) => {
+      setOnlineState(status === 'connected');
+    });
+    provider.awareness.setLocalState({
+      alphaColor: color.slice(0, -2) + '0.2)',
+      color,
+      name,
+    });
+    provider.on('sync', isSynced => {
+      console.log('sync', isSynced);
+      if (!isSynced) return;
+      // const ymap = ydoc.get(contenField)
+      const rawA = getRawBySharedData(contenFieldA, ydoc);
+      const rawB = getRawBySharedData(contenFieldB, ydoc);
+      if (rawA) {
+        setValueA(rawA);
+      } else {
+        const initRaw = valueA || rawContent;
+        setRawToSharedData(contenFieldA, ydoc, initRaw);
+      }
+      if (rawB) {
+        setValueB(rawB);
+      } else {
+        const initRaw = valueB || rawContent;
+        setRawToSharedData(contenFieldB, ydoc, initRaw);
+      }
+    });
+    provider.connect();
+    return () => {
+      draftBindA.destroy();
+      draftBindB.destroy();
+      provider.disconnect();
+    };
   }, []);
 
-  const onChange = () => {
-    // console.log('TeXEditorExample onChange');
-  };
-  return 'test';
-  // <TeXEditorExample
-  //   ref={editorRef}
-  //   draftBind={draftBind}
-  //   isOnline={isOnline}
-  //   onChange={onChange}
-  //   defaultValue={value}
-  // />
+  return (
+    <>
+      <TeXEditorExample
+        ref={editorRefA}
+        draftBind={draftBindA}
+        isOnline={isOnline}
+        defaultValue={valueA}
+      />
+      <TeXEditorExample
+        ref={editorRefB}
+        draftBind={draftBindB}
+        isOnline={isOnline}
+        defaultValue={valueB}
+      />
+    </>
+  );
 }
